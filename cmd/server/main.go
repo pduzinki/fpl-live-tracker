0aeb668f58b6c77ad5c9376a135470d672ace7c8
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2021-11-07 18:31:01 +0100

    add placeholder handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"fpl-live-tracker/pkg/http/rest"
 	"fpl-live-tracker/pkg/storage/memory"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello there, from fpl-live-tracker!")
+	fmt.Println("fpl-live-tracker started")
 
 	mr, err := memory.NewManagerRepository()
 	if err != nil {
 		panic(err)
 	}
 	_ = mr
+
+	router := rest.Handler()
+
+	log.Println("fpl-live-tracker now listening on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -1,6 +1,9 @@
 package rest
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,7 +12,9 @@ import (
 func Handler() http.Handler {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", nil).Methods("GET")
+	r.HandleFunc("/", Homepage()).Methods("GET")
+	r.HandleFunc("/api/manager/{id:[0-9]+}", GetManager()).Methods("GET")
+	r.HandleFunc("/api/league/{id:[0-9]+}", GetLeague()).Methods("GET")
 
 	return r
 }
@@ -18,6 +23,46 @@ func Handler() http.Handler {
 func Homepage() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		s := "fpl-live-tracker test"
 
+		j, err := json.Marshal(s)
+		if err != nil {
+			http.Error(w, "failed to marshal data", http.StatusInternalServerError)
+			return
+		}
+
+		io.Copy(w, bytes.NewReader(j))
+	}
+}
+
+//
+func GetManager() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		s := "fpl-live-tracker getmanager test"
+
+		j, err := json.Marshal(s)
+		if err != nil {
+			http.Error(w, "failed to marshal data", http.StatusInternalServerError)
+			return
+		}
+
+		io.Copy(w, bytes.NewReader(j))
+	}
+}
+
+//
+func GetLeague() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		s := "fpl-live-tracker getleague test"
+
+		j, err := json.Marshal(s)
+		if err != nil {
+			http.Error(w, "failed to marshal data", http.StatusInternalServerError)
+			return
+		}
+
+		io.Copy(w, bytes.NewReader(j))
 	}
 }

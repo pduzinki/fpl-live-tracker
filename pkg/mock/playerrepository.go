6ffa3dc52d2a5cc1b96bc240eacaafc26b9cd20e
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2022-01-30 15:10:25 +0100

    add first player service test

---
+++ pkg/mock/playerrepository.go
@@ -0,0 +1,26 @@
+package mock
+
+import "fpl-live-tracker/pkg/domain"
+
+type PlayerRepository struct {
+	AddFn         func(player domain.Player) error
+	UpdateFn      func(player domain.Player) error
+	UpdateStatsFn func(playerID int, stats domain.PlayerStats) error
+	GetByIDFn     func(ID int) (domain.Player, error)
+}
+
+func (pr *PlayerRepository) Add(player domain.Player) error {
+	return pr.AddFn(player)
+}
+
+func (pr *PlayerRepository) Update(player domain.Player) error {
+	return pr.UpdateFn(player)
+}
+
+func (pr *PlayerRepository) UpdateStats(playerID int, stats domain.PlayerStats) error {
+	return pr.UpdateStatsFn(playerID, stats)
+}
+
+func (pr *PlayerRepository) GetByID(ID int) (domain.Player, error) {
+	return pr.GetByIDFn(ID)
+}
--- pkg/services/player/player_test.go
+++ pkg/services/player/player_test.go
@@ -1,6 +1,16 @@
 package player
 
-import "testing"
+import (
+	"fpl-live-tracker/pkg/domain"
+	"fpl-live-tracker/pkg/mock"
+	"testing"
+)
+
+var ronaldo = domain.Player{
+	ID:       123,
+	Name:     "Ronaldo",
+	Position: "FWD",
+}
 
 func TestUpdate(t *testing.T) {
 	// TODO add test
@@ -11,7 +21,43 @@ func TestUpdateStats(t *testing.T) {
 }
 
 func TestGetByID(t *testing.T) {
-	// TODO add test
+	testcases := []struct {
+		playerID int
+		want     domain.Player
+		err      error
+	}{
+		{
+			playerID: 0,
+			want:     domain.Player{},
+			err:      ErrPlayerIDInvalid,
+		},
+		{
+			playerID: 123,
+			want:     ronaldo,
+			err:      nil,
+		},
+	}
+
+	pr := mock.PlayerRepository{
+		GetByIDFn: func(id int) (domain.Player, error) {
+			if id != 123 {
+				t.Fatalf("unexpected id")
+			}
+			return ronaldo, nil
+		},
+	}
+
+	ps := playerService{pr: &pr}
+
+	for _, test := range testcases {
+		got, err := ps.GetByID(test.playerID)
+		if err != test.err {
+			t.Errorf("error: want err %v, got %v", test.err, err)
+		}
+		if got != test.want {
+			t.Errorf("error: want %v, got %v", test.want, got)
+		}
+	}
 }
 
 func TestConvertToDomainPlayer(t *testing.T) {

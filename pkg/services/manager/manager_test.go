8421b29c27a879874d6a81100fbbcdfa2dcdbf19
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2022-02-12 17:50:27 +0100

    add test placeholders

--- pkg/services/manager/manager_test.go
+++ pkg/services/manager/manager_test.go
@@ -1,1 +1,27 @@
 package manager
+
+import "testing"
+
+func TestManagerServiceUpdateInfos(t *testing.T) {
+	// TODO add test
+}
+
+func TestManagerServiceUpdateTeams(t *testing.T) {
+	// TODO add test
+}
+
+func TestManagerServiceUpdatePoints(t *testing.T) {
+	// TODO add test
+}
+
+func TestManagerServiceGetByID(t *testing.T) {
+	// TODO add test
+}
+
+func TestManagerServiceConvertToDomainManager(t *testing.T) {
+	// TODO add test
+}
+
+func TestManagerServiceConvertToDomainTeam(t *testing.T) {
+	// TODO add test
+}

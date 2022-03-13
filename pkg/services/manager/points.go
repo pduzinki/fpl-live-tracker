f4f93bdf842ddf8a4b7927a7dd9222acecd5c5ac
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2022-03-13 16:26:56 +0100

    clear sub flags

--- pkg/services/manager/points.go
+++ pkg/services/manager/points.go
@@ -37,6 +37,12 @@ func calculateTotalPoints(team *domain.Team) int {
 func calculateSubPoints(team *domain.Team) int {
 	subPoints := 0
 
+	// clear sub flags
+	for i := 0; i < 15; i++ {
+		team.Picks[i].SubIn = false
+		// team.Picks[i].SubOut = false
+	}
+
 	if len(team.Picks) == 0 {
 		log.Println("calculateSubPoints: team has no players!")
 		return subPoints

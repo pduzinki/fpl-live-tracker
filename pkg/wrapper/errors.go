591085b228c04321ee3a741067cfdecea8c06965
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2022-03-16 22:22:18 +0100

    remove unneccesary error interface

--- pkg/wrapper/errors.go
+++ pkg/wrapper/errors.go
@@ -8,19 +8,14 @@ import (
 var ErrReadFailure error = errors.New("failed to read the response")
 var ErrUnmarshalFailure error = errors.New("failed to unmarshal data")
 
-type errorHttpNotOk struct {
+type ErrorHttpNotOk struct {
 	statusCode int
 }
 
-type ErrorHttpNotOk interface {
-	error
-	GetHttpStatusCode() int
-}
-
-func (err errorHttpNotOk) Error() string {
+func (err ErrorHttpNotOk) Error() string {
 	return fmt.Sprintf("http status not ok: %d\n", err.statusCode)
 }
 
-func (err errorHttpNotOk) GetHttpStatusCode() int {
+func (err ErrorHttpNotOk) GetHttpStatusCode() int {
 	return err.statusCode
 }

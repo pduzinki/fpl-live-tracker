b32114959b0498dfb55dad23b19f99b4b013d6c1
pawel duzinkiewicz <pawel.duzinkiewicz@live.com>
2022-05-26 21:01:21 +0200

    add mongo/team

---
+++ pkg/storage/mongo/team.go
@@ -0,0 +1,98 @@
+package mongo
+
+import (
+	"context"
+	"fmt"
+	"fpl-live-tracker/pkg/config"
+	"fpl-live-tracker/pkg/domain"
+	"fpl-live-tracker/pkg/storage"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
+)
+
+//
+type teamRepository struct {
+	db    *mongo.Database
+	teams *mongo.Collection
+}
+
+//
+func NewTeamRepository(config config.MongoConfig) (domain.TeamRepository, error) {
+	client, err := mongo.Connect(context.Background(),
+		options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%d", config.Host, config.Port)))
+	if err != nil {
+		return nil, err
+	}
+	// defer client.Disconnect(context.TODO())
+
+	db := client.Database(config.Database)
+	teams := db.Collection("team")
+
+	return &teamRepository{
+		db:    db,
+		teams: teams,
+	}, nil
+}
+
+//
+func (tr *teamRepository) Add(team domain.Team) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	_, err := tr.teams.InsertOne(ctx, team)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//
+func (tr *teamRepository) Update(ID int, team domain.Team) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	filter := bson.M{"_id": ID}
+
+	result, err := tr.teams.ReplaceOne(ctx, filter, team)
+	if err != nil {
+		return err
+	}
+
+	if result.MatchedCount == 0 {
+		return storage.ErrTeamNotFound
+	}
+
+	return nil
+}
+
+//
+func (tr *teamRepository) GetByID(ID int) (domain.Team, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	result := tr.teams.FindOne(ctx, bson.M{"_id": ID})
+
+	var team domain.Team
+	err := result.Decode(&team)
+	if err != nil {
+		return domain.Team{}, err
+	}
+
+	return team, nil
+}
+
+//
+func (tr *teamRepository) GetCount() (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	count, err := tr.teams.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		return 0, err
+	}
+
+	return int(count), nil
+}
